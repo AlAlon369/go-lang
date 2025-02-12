1fb7df2b3995c201b00c778b0e75486fcc864475
AlAlon369 <alalon211090@gmail.com>
2025-02-12 23:33:19 +0300

    Solved

--- main.go
+++ main.go
@@ -5,24 +5,22 @@ import (
 	"fmt"
 )
 
-// Тесты для проверки работы функции Triangle
 func main() {
 	tests := []struct {
-		input    string
-		expected rune
+		year     int
+		expected bool
 	}{
-		{"RRGBRGBB", 'G'},
-		{"RGB", 'B'},
-		{"R", 'R'},
-		{"GG", 'G'},
-		{"GB", 'R'},
-		{"BGR", 'G'},
+		{1600, true},  // Високосный (делится на 400)
+		{1700, false}, // Не високосный (делится на 100, но не на 400)
+		{2000, true},  // Високосный (делится на 400)
+		{2024, true},  // Високосный (делится на 4, но не на 100)
+		{2023, false}, // Не високосный (не делится на 4)
 	}
 
 	for i, test := range tests {
-		result := CodeWars.Triangle(test.input)
-		fmt.Printf("Test %d: Input: %s, Expected: %c, Got: %c\n",
-			i+1, test.input, test.expected, result)
+		result := CodeWars.IsLeapYear(test.year)
+		fmt.Printf("Test %d: Year: %d, Expected: %t, Got: %t\n",
+			i+1, test.year, test.expected, result)
 
 		if result != test.expected {
 			fmt.Printf("❌ Test %d failed!\n\n", i+1)
